eb7375bcc346d26e96aed625a7153549148a6d95
scubamike <scubamike+git@gmail.com>
2022-12-09 09:28:55 -0500

    Testing

---
+++ prime-test.ps1
@@ -0,0 +1,10 @@
+$p=7
+$prime=$false
+
+for ($i=2;$i -lt ($p);$i++) {
+ 	if (($p % $i) -eq 0) {
+	$prime=$true
+	}
+
+}
+if ($prime -eq $true) {write-output "Not Prime" } else {write-output "PRIME"}
